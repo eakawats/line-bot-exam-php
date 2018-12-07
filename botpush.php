a0457f649bdd2c077557355ad9f8d355852e6dac
eakawats <38580850+eakawats@users.noreply.github.com>
2018-12-07 10:41:40 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -6,7 +6,7 @@ $access_token = 'OT5CR0YWH4DUiCp3duCWPArMVPQJkebSmBzhptk6y+juD0wG1AqDTDwda7kdVsa
 
 $channelSecret = '41876669dc9e269ce3b2c5e26bbdf5da';
 
-$pushID = 'U2fbe10562abe2aad0d5ff6c96db6f0ea';
+$pushID = 'Uac0a8ff6cf95a9475a45bffc13d1814f';
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
@@ -14,8 +14,8 @@ $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
 $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello world');
 $response = $bot->pushMessage($pushID, $textMessageBuilder);
 
-echo "test";
-//echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
+//echo "test";
+echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
 
 ?>
 
