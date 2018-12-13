62e0a46e4b68d4c123fab37d4f88bb875de1e449
eakawats <38580850+eakawats@users.noreply.github.com>
2018-12-13 15:33:00 +0700

    Update pushmsg.php

--- pushmsg.php
+++ pushmsg.php
@@ -19,6 +19,16 @@
       $arrayPostData['messages'][1]['stickerId'] = "34";
       pushMsg($arrayHeader,$arrayPostData);
    }
+   
+if($message == "พิกัดสยามพารากอน"){
+       // $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
+        $arrayPostData['messages'][0]['type'] = "location";
+        $arrayPostData['messages'][0]['title'] = "สยามพารากอน";
+        $arrayPostData['messages'][0]['address'] =   "13.7465354,100.532752";
+        $arrayPostData['messages'][0]['latitude'] = "13.7465354";
+        $arrayPostData['messages'][0]['longitude'] = "100.532752";
+        replyMsg($arrayHeader,$arrayPostData);
+    }
 
    if($message == "นับ 1-10"){
        for($i=1;$i<=10;$i++){
