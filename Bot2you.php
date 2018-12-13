c98fa115b66581f1958f373f4b87aa9295ebfba7
eakawats <38580850+eakawats@users.noreply.github.com>
2018-12-14 02:14:47 +0700

    Update Bot2you.php

--- Bot2you.php
+++ Bot2you.php
@@ -26,8 +26,9 @@
         replyMsg($arrayHeader,$arrayPostData);
     }
     #ตัวอย่าง Message Type "Image"
-    else if($message == "รูปน้องแมว"){
-        $image_url = "https://i.pinimg.com/originals/cc/22/d1/cc22d10d9096e70fe3dbe3be2630182b.jpg";
+    else if($message == "รูปภรรยา"){
+       // $image_url = "https://i.pinimg.com/originals/cc/22/d1/cc22d10d9096e70fe3dbe3be2630182b.jpg";
+       $image_url = "https://www.facebook.com/photo.php?fbid=1914304808589314&set=a.468401249846351&type=3&theater"
         $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
         $arrayPostData['messages'][0]['type'] = "image";
         $arrayPostData['messages'][0]['originalContentUrl'] = $image_url;
