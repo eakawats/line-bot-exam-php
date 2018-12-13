feee19c1e8102fd06eae2fc15ca4b6bb3d1dbc7d
eakawats <38580850+eakawats@users.noreply.github.com>
2018-12-13 23:46:53 +0700

    Update botbot.php

--- botbot.php
+++ botbot.php
@@ -27,15 +27,15 @@ if($arrJson['events'][0]['message']['text'] == "สวัสดี"){
   $arrPostData['messages'][0]['type'] = "text";
   $arrPostData['messages'][0]['text'] = "ฉันทำอะไรไม่ได้เลย คุณต้องสอนฉันอีกเยอะ";
 }
-/*else if($arrayJson['events'][0]['replyToken' == "พิกัดสยามพารากอน"){
-       //$arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
+else if($arrJson['events'][0]['message']['text'] == "พิกัดสยามพารากอน"){
+       $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
         $arrayPostData['messages'][0]['type'] = "location";
         $arrayPostData['messages'][0]['title'] = "สยามพารากอน";
         $arrayPostData['messages'][0]['address'] =   "13.7465354,100.532752";
         $arrayPostData['messages'][0]['latitude'] = "13.7465354";
         $arrayPostData['messages'][0]['longitude'] = "100.532752";
        // replyMsg($arrayHeader,$arrayPostData);
-    }*/
+    }
 else{
   $arrPostData = array();
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
