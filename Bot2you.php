51ef88d639b99a76f57798fc05c21d5744838c5e
eakawats <38580850+eakawats@users.noreply.github.com>
2018-12-19 15:37:14 +0700

    Update Bot2you.php

--- Bot2you.php
+++ Bot2you.php
@@ -27,7 +27,7 @@
     }
     #ตัวอย่าง Message Type "Image"
     else if($message == "รูป"){
-        $image_url = 'https://onedrive.live.com/?authkey=%21ANOSc1lq8ew2rAE&cid=384A686EC0894FCF&id=384A686EC0894FCF%217002&parId=384A686EC0894FCF%215352&o=OneUp';
+        $image_url = 'https://www.dropbox.com/s/yo0e2qtv5madpnl/316424.jpg?dl=0';
        //$image_url = "https://www.picz.in.th/image/2.9zptwa";
         $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
         $arrayPostData['messages'][0]['type'] = "image";
