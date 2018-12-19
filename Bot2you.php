14bdf5ba7eef44f38224f8d7634e99f80d07e2f9
eakawats <38580850+eakawats@users.noreply.github.com>
2018-12-19 10:01:54 +0700

    Update Bot2you.php

--- Bot2you.php
+++ Bot2you.php
@@ -49,7 +49,7 @@
     else if($message == "ลาก่อน"){
         $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
         $arrayPostData['messages'][0]['type'] = "text";
-        $arrayPostData['messages'][0]['text'] = "อย่าทิ้งกันไป";
+        $arrayPostData['messages'][0]['text'] = "อย่าทิ้งกันไปนะ";
         $arrayPostData['messages'][1]['type'] = "sticker";
         $arrayPostData['messages'][1]['packageId'] = "1";
         $arrayPostData['messages'][1]['stickerId'] = "131";
